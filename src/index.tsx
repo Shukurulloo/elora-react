535594574243732afa4546bc7dbb377f1e93a04d
mac <mac@macui-MacBookPro.local>
2024-03-24 16:12:21 +0900

    BRR: Initial setup

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+// birinchi loyihamz shuyerdan boshlanadi
+import React from 'react';
+import ReactDOM from 'react-dom'; // bu package vertual va real domlarni integratsiyasini qiladi
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './app/App';
+import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+
+ReactDOM.render(  // ReactDOM olib ustida renderni amalga oshirib ichiga 1-ument react srcni joylaymiz
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+/** vertdomga realdomni instansi yani documentni functsiyasini root qismi 2-argumnet qlb pass qilamz*/
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
