e1b118b2edd3b2269ea9b929de81508c84608301
mac <mac@macui-MacBookPro.local>
2024-04-22 16:28:05 +0900

    feat: create homePage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistic";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -8,7 +8,14 @@ import Events from "./Events";
 import "../../../css/home.css"
 
 export default function HomePage() {
+    // Selector: Store => Data
+
+    useEffect(() => {
+        // Backent server data request => Data
+        // slice: Data => Store 
+    }, []);
     
+
     return <div className={"homepage"}>
         <Statistics/>
         <PopularDishes/>
