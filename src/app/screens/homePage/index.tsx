dd100d262856693f13ccf96d59db039ea76b1b44
mac <mac@macui-MacBookPro.local>
2024-04-24 16:13:51 +0900

    feat: develop product service business logic to retrieve data from backend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,30 +7,38 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css"
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE & SELECTOR **/
 // dispatch orqali data reducerga kirb keladi
 const actionDispatch = (dispatch: Dispatch) => ({ //vareblga tenglab dispatchni typesi bn kiritib unga 1ta argument
     setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });// setPopularDishes comandasini setPopularDishes reducer orqli amalga oshrmiz
-const popularDishesRetriever = createSelector( //yuklangan datani retriever orqali qabul qilish
-    retrievePopularDishes, // 1-argument
-    (popularDishes) => ({ popularDishes }) // 2-argument
-);
+
 
 export default function HomePage() {
     // actionDispatchni icidan useDispatchni kiritsak bizga setPopularDishes comondasini functiona compni ichiga chaqiradi
     const {setPopularDishes} = actionDispatch(useDispatch()); 
-    const {popularDishes} = useSelector(popularDishesRetriever);  //useSelector hookiga pas qilib popularDishesni qolga olamz
 
-
-    useEffect(() => {}, []); // 3xil lifesicle
+    useEffect(() => {
+        // Backend server data fetch => Data
+        const product = new ProductService();
+        product.getProducts({
+            page: 1,
+            limit: 4,
+            order: "productViews",
+            productCollection: ProductCollection.DISH,
+        })
+        .then(data => {
+            setPopularDishes(data);
+        })
+        .catch(err => console.log(err));
+    }, []); // 3xil lifesicle
 
     return <div className={"homepage"}>
         <Statistics/>
