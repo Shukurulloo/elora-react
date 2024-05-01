284bab7a982bdcf35e95daf54153aaee0c17aeb4
mac <mac@macui-MacBookPro.local>
2024-05-01 20:27:59 +0900

    feat: develop updateMember business logic and modify ordersPage component

--- src/app/context/ContextProvider.tsx
+++ src/app/context/ContextProvider.tsx
@@ -19,9 +19,12 @@ const ContextProvider: React.FC<{children: ReactNode}> = ({children}) => {
     const [orderBuilder, setOrderBuilder] = useState<Date>(new Date());
     console.log("=== verify ===");
 
-    return (<GlobalContext.Provider value={{authMember, setAuthMember, orderBuilder, setOrderBuilder}}>
+    return (<GlobalContext.Provider 
+        value={{authMember, setAuthMember, orderBuilder, setOrderBuilder}}
+        >
         {children}  
-        </GlobalContext.Provider>)
+        </GlobalContext.Provider>
+    );
 };
 
 export default ContextProvider
