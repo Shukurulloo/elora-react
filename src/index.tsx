0010d16d50e2d646bf481e5dd565d9d1099e1385
mac <mac@macui-MacBookPro.local>
2024-08-13 22:02:03 +0900

    feat: develop Sockte.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,37 @@
 // birinchi loyihamz shuyerdan boshlanadi
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // bu package vertual va real domlarni integratsiyasini qiladi
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
-import CssBaseline from '@mui/material/CssBaseline'; // standart integratsiya
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './app/MaterialTheme';
-import { BrowserRouter as Router} from "react-router-dom"; // rooting tizimini shu package . BrowserRouterni Routerga o'zgartiramz
-import './css/index.css';
-import ContextProvider from './app/context/ContextProvider';
+import React from "react";
+import { createRoot } from "react-dom/client"; // bu package vertual va real domlarni integratsiyasini qiladi
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline"; // standart integratsiya
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom"; // rooting tizimini shu package . BrowserRouterni Routerga o'zgartiramz
+import ContextProvider from "./app/context/ContextProvider";
+import { SocketProvider } from "./app/context/SocketContext";// SocketProviderni itegratsia qilamz
+import "./css/index.css";
 
 const container = document.getElementById("root")!; // "!" non-null belgisi
 const root = createRoot(container);
 
-root.render(//(vertualdom) ReactDOM olib ustida renderni amalga oshirib ichiga 1-argument react(vertualdom) srcni joylaymiz
+root.render(
+  //(vertualdom) ReactDOM olib ustida renderni amalga oshirib ichiga 1-argument react(vertualdom) srcni joylaymiz
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
-      <ThemeProvider theme={theme}> 
-        <CssBaseline />
-          <Router>
-             <App />
-          </Router>
-      </ThemeProvider>
+        <SocketProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+              <App />
+            </Router>
+          </ThemeProvider>
+        </SocketProvider>
       </ContextProvider>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
